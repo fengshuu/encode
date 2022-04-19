1e3a8d62dac16152e2d551ce1201c7e09a0bf9d2
bli <bli@qq.com>
2022-04-19 13:10:03 +0800

    Revert "add proxy"
    This reverts commit 5c60dcdca988ff3d9d64cfc57a8133037d14ff18.

--- sayhello/__init__.py
+++ sayhello/__init__.py
@@ -5,40 +5,15 @@
     :copyright: © 2018 Grey Li <withlihui@gmail.com>
     :license: MIT, see LICENSE for more details.
 """
-from flask import Flask, render_template, request
+from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-
-class ReverseProxied(object):
-    '''Wrap the application in this middleware and configure the
-    front-end server to add these headers, to let you quietly bind
-    this to a URL other than / and to an HTTP scheme that is
-    different than what is used locally.
-
-    :param app: the WSGI application
-    '''
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
-        if script_name:
-            environ['SCRIPT_NAME'] = script_name
-            path_info = environ['PATH_INFO']
-            if path_info.startswith(script_name):
-                environ['PATH_INFO'] = path_info[len(script_name):]
-
-        scheme = environ.get('HTTP_X_SCHEME', '')
-        if scheme:
-            environ['wsgi.url_scheme'] = scheme
-        return self.app(environ, start_response)
 
 app = Flask('sayhello')
 app.config.from_pyfile('settings.py')
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
-app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
